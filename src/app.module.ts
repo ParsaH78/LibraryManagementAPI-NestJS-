f28ecd9ed19b6840cb1196a9b4e871649d190dea
parsa <parsa.hsn78@gmail.com>
2023-09-13 18:28:29 +0330

    things added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
+import { APP_INTERCEPTOR, APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/auth.guard';
+import { UserInterceptor } from './user/interceptors/user.interceptor';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule],
+  imports: [AuthModule, PrismaModule, UserModule, BookModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: UserInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
